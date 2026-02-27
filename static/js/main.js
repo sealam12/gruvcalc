ff6fd316ac68186aaae6d042ccadf3a4870cbdab
Matthew Carmichael <sealblox@gmail.com>
2026-02-27 21:09:40 +0000

    Edited CSS, restarted js

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,4 @@
-class Gruvcalc {
-    constructor() {
-        this.inputElement = $("#input");
-        this.prefixElement = $("#prefix");
-        this.previewElement = $("#preview");
-    }
-
-    resetStyling() {
-        this.prefixElement.attr("className", "eval");
-        this.inputElement.attr("className", "input eval");
-    }
-
-    init() {
-        
-    }
-}
-
 window.onerror = function (message, url, lineNo, columnNo, error) {
     document.body.innerHTML = `${message} at line ${lineNo}:${columnNo}`;
     return true;
 };
-
-window.gruvcalc = new Gruvcalc();
-window.gruvcalc.init();
