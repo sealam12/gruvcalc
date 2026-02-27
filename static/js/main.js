eabcbfce01698cfcc31feeb03a2b3301cba118bd
Matthew Carmichael <sealblox@gmail.com>
2026-02-27 22:08:57 +0000

    Started core functionality, fixed input autocorrect

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,90 @@
+const modes = {
+    "normal": {
+        hotkey: "Escape",
+        prefix: "=",
+        color: "var(--color-primary)"
+    },
+
+    "command": {
+        hotkey: "\\",
+        prefix: ">",
+        color: "var(--color-secondary)"
+    },
+
+    "hotkey": {
+        hotkey: null,
+        prefix: "@",
+        color: "var(--color-success)"
+    }
+}
+
+let mode = modes.normal;
+
+const inputContainer = $("#input-container");
+const inputPrefix = $("#input-prefix");
+const input = $("#input-box");
+
+const outputPreview = $("#output-preview");
+
+function setPrefix(newPrefix) {
+    inputPrefix.text(newPrefix);
+}
+
+function switchMode(newMode) {
+    setPrefix(newMode.prefix);
+    inputContainer.css("--INPUT-accent", newMode.color);
+    input.focus();
+    input.val("");
+
+    mode = newMode;
+}
+
+async function onKeydown(event) {
+    if (!input.is(":focus") && event.key == "f") {
+        event.preventDefault();
+        input.focus();
+        return;
+    }
+
+    const previousValue = input.val();
+
+    for (const [modeName, mode] of Object.entries(modes)) {
+        if (event.key === mode.hotkey) {
+            event.preventDefault();
+            switchMode(mode);
+        }
+
+        if (previousValue.trim() == "" && event.key === mode.prefix) {
+            event.preventDefault();
+            switchMode(mode);
+        }
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    let evaluated;
+    let color = "var(--color-primary)";
+    const currentVal = input.val();
+    
+    try {
+        evaluated = eval(currentVal);
+        color = evaluated != undefined ? "var(--color-success)" : "var(--color-primary)";
+        evaluated = evaluated != undefined ? evaluated.toString() : "Type an expression to view it's output";
+    } catch (error) {
+        evaluated = error.message;
+        color = "var(--color-error)";
+    }
+
+    if (evaluated != undefined) {
+        outputPreview.text(evaluated);
+        outputPreview.css("--PREVIEW-accent", color);
+    }
+}
+
+$(window).on("keydown", function(event) {
+    onKeydown(event);
+})
+
 window.onerror = function (message, url, lineNo, columnNo, error) {
     document.body.innerHTML = `${message} at line ${lineNo}:${columnNo}`;
     return true;
